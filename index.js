c6c8058fe50cc43d277d53a7e09db81113813afb
JanRov <honza.rovny@gmail.com>
2023-10-26 11:38:01 +1100

    Second commit, form working as intended

--- index.js
+++ index.js
@@ -6,53 +6,61 @@ const app = express();
 const port = 3000;
 const API_KEY = "efe6f6243a23432091834636b8881af7";
 
-
-app.use(express.static('public'))
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-
-
-
-app.get  ("/", async (req, res) => {
-    try {
-    const response = await axios.get("https://timezone.abstractapi.com/v1/current_time", {
+/*
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://timezone.abstractapi.com/v1/current_time",
+      {
         params: {
           api_key: API_KEY,
-          location: "",
+          location: "Los Angeles, CA",
         },
-      });
-    res.render("index.ejs", { time: response.data.datetime, timezone: response.data.timezone_location });
+      }
+    );
+    res.render("index.ejs", {
+      time: response.data.datetime,
+      timezone: response.data.timezone_location,
+    });
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs");
   }
-  });
+});
+*/
 
-  app.get("/submit", async(req,res) =>{
-    try {
-      const response = await axios.get("https://timezone.abstractapi.com/v1/current_time", {
-          params: {
-            api_key: API_KEY,
-            location: req.body.location,
-          },
-        });
-      console.log(req.data.location);
-      res.render("index.ejs", { time: response.data.datetime, timezone: response.data.timezone_location, place: requested_location });
-    } catch (error) {
-      console.error("Failed to make request:", error.message);
-      res.render("index.ejs", {error: error.message});
-    }
+/*
+app.get("/", (req, res) => {
+  res.render("index.ejs");
+});
 
-
-
-
-  });
-
-
-
-
-
-
-
+app.post("/submit", (req,res)=>{
+  let location= req.body.location;
+});
+*/
+app.get("/", async (req, res) => {
+  try {
+    const response = await axios.get(
+      "https://timezone.abstractapi.com/v1/current_time",
+      {
+        params: {
+          api_key: API_KEY,
+          location: req.query.location,
+        },
+      }
+    );
+    res.render("index.ejs", {
+      timezone_name: response.data.timezone_name,
+      datetime: response.data.datetime,
+      requested_location: response.data.requested_location,
+    });
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", { error: error.message, message: "Please choose a location." });
+  }
+});
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
-  });
+  console.log(`Server is running on port ${port}`);
+});
